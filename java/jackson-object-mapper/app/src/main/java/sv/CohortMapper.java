ef93a99a1e2b7f272025b8edff08635686d0871f
Sumanth Vepa <svepa@milestone42.com>
2023-11-22 11:02:42 +0530

    [issue003] [De]Serialize complex classes w/ Jackson ObjectMapper
    FEATURE(s):
    [issue003] jackson-object-mapper:
      Use Jackson's Object Mapper to serialize and deserialize a complex
      class.
    
    FILE(s):
    [modified].gitignore
    [modified]TODO.txt
    [new]java/jackson-object-mapper/.gitattributes
    [new]java/jackson-object-mapper/.gitignore
    [new]java/jackson-object-mapper/.idea/.gitignore
    [new]java/jackson-object-mapper/.idea/compiler.xml
    [new]java/jackson-object-mapper/.idea/gradle.xml
    [new]java/jackson-object-mapper/.idea/inspectionProfiles/Project_Default.xml
    [new]java/jackson-object-mapper/.idea/jarRepositories.xml
    [new]java/jackson-object-mapper/.idea/misc.xml
    [new]java/jackson-object-mapper/.idea/vcs.xml
    [new]java/jackson-object-mapper/app/build.gradle
    [new]java/jackson-object-mapper/app/src/main/java/sv/CohortMapper.java
    [new]java/jackson-object-mapper/app/src/main/java/sv/Person.java
    [new]java/jackson-object-mapper/app/src/test/java/sv/CohortMapperTest.java
    [new]java/jackson-object-mapper/gradle/wrapper/gradle-wrapper.jar
    [new]java/jackson-object-mapper/gradle/wrapper/gradle-wrapper.properties
    [new]java/jackson-object-mapper/gradlew
    [new]java/jackson-object-mapper/gradlew.bat
    [new]java/jackson-object-mapper/settings.gradle
    
    DESCRIPTION
    Use Jackson's Object Mapper to serialize and deserialize a complex
    class.
    The code creates a Person class and a List of Persons and uses
    ObjectMapper to serialize and deserialize them.

---
+++ java/jackson-object-mapper/app/src/main/java/sv/CohortMapper.java
@@ -0,0 +1,90 @@
+package sv;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import org.jetbrains.annotations.NotNull;
+
+
+public class CohortMapper {
+  private final ObjectMapper mapper;
+
+  public CohortMapper() {
+    this.mapper = new ObjectMapper();
+  }
+
+  public @NotNull String serialize(Person person)
+      throws JsonProcessingException {
+    return mapper.writeValueAsString(person);
+  }
+
+  public @NotNull Person deserialize(String json)
+      throws JsonProcessingException {
+    return mapper.readValue(json, Person.class);
+  }
+
+  public @NotNull String serialize(List<Person> persons)
+      throws JsonProcessingException {
+    return mapper.writeValueAsString(persons);
+  }
+
+  public @NotNull ArrayList<Person> deserializeList(String json)
+      throws JsonProcessingException {
+    /*
+      The problem with deserializing to a generic type is that you
+      cannot call ArrayList<Person>.class at runtime. All generic type
+      information is erased at runtime. You only have ArrayList.class
+      which is a list of objects. If we used that method, the type
+      signature of deserializeList would have to be @NotNull
+      ArrayList. Which would cause syntax issues at compile time. The
+      solution is to create a special object JavaType provided by the
+      jackson library to capture both the type of the collection and
+      the type of its elements. This class contains methods that
+      allows See this StackOverflow thread for the technique
+      mapper.readValue function to create list of the correct type.
+      The return value is correct at compile time. (Exactly how this
+      works is not clear to me yet.)
+      See this link for details:
+      https://stackoverflow.com/questions/6846244/jackson-and-generic-type-reference
+
+      Also take a look at:
+      https://www.baeldung.com/guava-reflection
+      https://github.com/google/guava/wiki/ReflectionExplained
+      https://guava.dev/releases/21.0/api/docs/com/google/common/reflect/TypeToken.html
+      https://github.com/google/guava
+      https://stackoverflow.com/questions/18471701/passing-a-class-with-type-parameter-as-type-parameter-for-generic-method-in-java
+
+
+
+
+
+
+
+     */
+    JavaType type
+        = mapper.getTypeFactory().constructCollectionType(
+            ArrayList.class, Person.class);
+    return mapper.readValue(json, type);
+  }
+
+  public static void main(String[] args) {
+    try {
+      var cohortMapper = new CohortMapper();
+      var firstPerson = new Person("First Person", 52);
+      var secondPerson = new Person("Second Person", 32);
+      var persons = new ArrayList<Person>();
+      persons.add(firstPerson);
+      persons.add(secondPerson);
+
+      System.out.println(cohortMapper.serialize(firstPerson));
+      System.out.println(cohortMapper.serialize(persons));
+
+    } catch (Exception ex) {
+      ex.printStackTrace(System.err);
+    }
+  }
+}
---
+++ java/jackson-object-mapper/app/src/main/java/sv/Person.java
@@ -0,0 +1,3 @@
+package sv;
+
+public record Person(String name, int age) {}
