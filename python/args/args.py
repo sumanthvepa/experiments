bcc9fe667aff6fde2bf07b281721f93da9934f8d
Sumanth Vepa <svepa@milestone42.com>
2024-02-12 10:53:15 +0530

    [issue008] args: Add README, COPYING and Copyleft notices
    FEATURE(s):
    [issue008] python-args: Print all the command line args
    
    FILE(s):
    [new]python/args/COPYING:
      The GNU General Public License Version 3
    [new]python/args/README:
      Instructions to run the script
    [modified]python/args/args.py:
      Add GNU copyright notice
    
    DESCRIPTION
    Add README, COPYING and Copyleft notices

--- python/args/args.py
+++ python/args/args.py
@@ -2,6 +2,24 @@
 """
   args.py: This script prints out all the arguments passed to it.
 """
+#--------------------------------------------------------------------
+# args.py: A python script the prints all the arguments passed to it
+#
+# Copyright (C) 2024 Sumanth Vepa.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#--------------------------------------------------------------------
 import sys
 
 
