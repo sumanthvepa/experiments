73219bc5cbb189e89bbf5b9a336c58e6e7645a45
Sumanth Vepa <svepa@milestone42.com>
2024-03-10 07:33:17 +0530

    [issue018] javascript-basics-02-cli:: A CLI program using commander
    FEATURE(s):
    [issue018] javascript-basics-02-cli:
      A CLI program that demonstrates the use of commander to get a
      port number from the command line.
    
    FILE(s)
    [modified]TODO.txt: Add issue018
    [new]javascript/basics/02-cli/.gitignore:
      Git ignore file for the cli directory. Ignore node_modules.
    [new]javascript/basics/02-cli/.idea/.gitignore:
      Git ignore file for IntelliJ IDEA project directory.
      Ignore InteliJ IDE artifacts that should not be committed to
      version control.
    [new]javascript/basics/02-cli/.idea/02-cli.iml:
      IntelliJ IDEA module file
    [new]javascript/basics/02-cli/.idea/inspectionProfiles/
        Project_Default.xml:
      IntelliJ IDEA inspection profile
    [new]javascript/basics/02-cli/.idea/jsLibraryMappings.xml:
      IntelliJ JavaScript library mappings
    [new]javascript/basics/02-cli/.idea/misc.xml:
      IntelliJ IDEA miscellaneous settings file
    [new]javascript/basics/02-cli/.idea/modules.xml:
      IntelliJ IDEA modules file
    [new]javascript/basics/02-cli/.idea/vcs.xml:
      IntelliJ IDEA version control system settings file
    [new]javascript/basics/02-cli/COPYING:
      GNU Public License Version 3
    [new]javascript/basics/02-cli/cli.mjs:
      Use the commander package to allow a port number to be specified on
      the command line using the --port or -p option.
    [new]javascript/basics/02-cli/package-lock.json:
      NPM package lock file
    [new]javascript/basics/02-cli/package.json
      NPM package file.
    
    DESCRIPTION
    javascript-basics-02-cli:: A CLI program using commander
    
    Takes --port or -p to specify the port number. If the option is not
    present, the port number defaults to 8080. Prints the port number
    along with any other arguments.
    
    The purpose of this commit is to explore the logic that will be needed
    to specify a port number on the command line to a nodejs server
    program.

---
+++ javascript/basics/02-cli/cli.mjs
@@ -0,0 +1,53 @@
+/* -------------------------------------------------------------------
+ * 02-cli.mjs: Print command line arguments.
+ *
+ * Copyright (C) 2024 Sumanth Vepa.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------*/
+// Commander is a CLI library to process command line options.
+// You can find it's manual page here:
+// https://www.npmjs.com/package/commander?activeTab=readme.
+// To use this code you have to install commander first:
+// $ npm install commander
+import { Command } from 'commander';
+
+const program = new Command();
+
+// Each call to a method on program returns the program object itself.
+// This allows for chaining method calls. The final parse() method
+// uses process.argv implicitly.
+program
+    .version('1.0.0')
+    .description('Print command line arguments.')
+    .option('-p, --port, <port>', 'Port number to listen on.', '8080')
+    .parse();
+
+// The opts() method returns an object containing the options
+// that were parsed from the command line. The args array
+// contains the remaining command line arguments.
+// Attributes of the options object are named after the long
+// option names. The value of each attribute is the value
+// of the option if it was specified on the command line.
+// Otherwise, the value is the default value specified in
+// the call to the option() method.
+const options = program.opts();
+const port = options.port;
+const params = program.args;
+
+// Note the use of .join(' ') to concatenate the elements of the
+// array into a single string.
+console.log(`Listening on port ${port}`);
+console.log(`Remaining command line arguments: ${params.join(' ')}`);
