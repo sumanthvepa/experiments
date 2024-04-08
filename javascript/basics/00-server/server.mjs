6acbb35b3bccd8116f7b8dd6fcdee7c074d10af0
Sumanth Vepa <svepa@milestone42.com>
2024-04-08 11:06:40 +0530

    [issue016] Add more explanatory comments to the code
    FEATURE(s):
    [issue016] javascript-node-:
      A simple server that reutrns Hello World as plain text
    
    FILE(s)
    [modified]javascript/basics//server.mjs: Add more explanatory
    comments to the code
    
    DESCRIPTION
    Add more explanatory comments to the code

--- javascript/basics/00-server/server.mjs
+++ javascript/basics/00-server/server.mjs
@@ -19,6 +19,14 @@
  * <https://www.gnu.org/licenses/>.
  -------------------------------------------------------------------*/
 
+// A note on comments
+// The comments for programs in this series are intended to be read
+// as teaching notes rather than as standard comments. They are
+// are far more extensive than typical comments, as they are intended
+// for educational purposes. Real code would be commented differently.
+// Read the comments alongside the code to get the most out of them.
+
+
 // Import the http module.
 //  Note that we are importing the http module using the ES6 import
 //  syntax. We do this because the import syntax is part of the
@@ -29,25 +37,52 @@
 //  To use the import syntax, we need to use the .mjs file extension.
 //  Alternately, we could specify "type": "module" in the package.json
 //  file, but we prefer to use the .mjs file extension.
-import http from 'http';
+import http from 'node:http';
 
 // The hostname is set to local host for safety. The servers in
 // these lessons, although generally safe, are not explicitly designed
 // for the open web.
 const hostname = '127.0.0.1';
 
-// Servers in this series use  ports starting at 9000. The port number
+// Servers in this series use ports starting at 9000. The port number
 // is related to the  lesson number. So if lesson number 23 has a
 // server component, it will use port 9023. This makes it easy to
 // figure out which port a given lesson's server is using.
 const port = 9000;
 
+// The createServer function is documented at:
+// https://nodejs.org/docs/latest-v20.x/api/http.html#httpcreateserveroptions-requestlistener
+// The request listener is a function that is called when the server receives
+// a request via a 'request' event. The request listener is passed two
+// arguments: a request object and a response object.
+// The request object is an instance of the http.IncomingMessage class.
+// The response object is an instance of the http.ServerResponse class.
 const server = http.createServer((req, res) => {
+  console.log('typeof req:', typeof req);
+  console.log('detailed type of req:', Object.prototype.toString.call(req));
+  console.log('class of req:', req.constructor.name);
+
+  console.log('typeof res:', typeof res);
+  console.log('detailed type of res:', Object.prototype.toString.call(res));
+  console.log('class of res:', res.constructor.name);
+
+  // To generate a response to the client, we use the response object's
+  // setHeader and end methods. The setHeader method sets the value of a
+  // response header. The end method sends the response headers and body
+  // to the client. The end method takes an optional argument that is
+  // used as the response body. If no argument is provided, the response
+  // body is empty.
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello, World!\n');
 });
 
+// The server listens on the specified port and hostname. When the server
+// is ready to accept connections, the callback function is called.
+// Internally, this callback is implemented as a listener on the
+// 'listening' event that is emitted by the server when it starts
+// listening. Complete documentation can be found here:
+// https://nodejs.org/docs/latest-v20.x/api/http.html#class-httpserver
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`);
 });
