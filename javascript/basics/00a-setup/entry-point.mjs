815e8b437fe8fb0c4b78fa478967fe8c09929cb3
Sumanth Vepa <svepa@milestone42.com>
2024-04-08 17:17:55 +0530

    [issue026] Sample project showing how to setup/manage an NPM project
    FEATURE(s):
    [issue026] javascript-basics-00a-setup:
      Sample project showing how to setup/manage an NPM project
    
    FILE(s)
    [modified].gitignore: Ignore vim .swp files
    [modified]TODO.txt: Add issue026
    [new]javascript/basics/00a-setup/.gitignore:
      Ingore node/npm project artifacts
    [new]javascript/basics/00a-setup/.idea/.gitignore
      Ignore IntelliJ IDEA non-commitable project artifacts
    [new]javascript/basics/00a-setup/.idea/00a-setup.iml
      IntelliJ IDEA project file
    [new]javascript/basics/00a-setup/.idea/inspectionProfiles/Project_Default.xml
      IntelliJ IDEA default inspection profile.
    [new]javascript/basics/00a-setup/.idea/misc.xml
      IntelliJ IDEA miscellaneous settings
    [new]javascript/basics/00a-setup/.idea/modules.xml
      IntelliJ IDEA modules file
    [new]javascript/basics/00a-setup/.idea/vcs.xml
      IntelliJ IDEA version control system settings
    [new]javascript/basics/00a-setup/COPYING
      GNU GPL 3.0 copyright notice
    [new]javascript/basics/00a-setup/README.md
      Description of how to setup a node/npm project
    [new]javascript/basics/00a-setup/entry-point.mjs
      A dummy javascript file to illustrate setting up
      of a node/npm project
    [new]javascript/basics/00a-setup/package-lock.json
      NPM package lock file
    [new]javascript/basics/00a-setup/package.json
      NPM package file
    
    DESCRIPTION
    Sample project showing how to setup/manage an NPM project.
    
    Use this project to learn how setup NPM projects.

---
+++ javascript/basics/00a-setup/entry-point.mjs
@@ -0,0 +1,53 @@
+/* -------------------------------------------------------------------
+ * 00a-setup.mjs: A dummy file for use in a demonstration of setting
+ * up a node project using npm.
+ *
+ * Copyright (C) 2024 Sumanth Vepa.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------*/
+
+
+// A note on comments
+// The comments for programs in this series are intended to be read
+// as teaching notes rather than as standard comments. They are far
+// more extensive than typical comments, as they are intended
+// for educational purposes. Real code would be commented differently.
+// Read the comments alongside the code to get the most out of them.
+
+
+// This part of a sample project that demonstrates how to set up
+// a node project. See the README.md file for complete instructions.
+
+
+// Import various modules.
+// See 00-server for an explanation of how ES6 imports work and
+// why they should be preferred over the commonJS require.
+// In this context the imports serve no real purpose.
+
+// Note on noinspection comments
+// These suppress warnings from IntelliJ IDEA's linter. They are
+// not necessary in a real program, but are used here to avoid
+// cluttering the code with warnings.
+
+// noinspection ES6UnusedImports
+import http from 'node:http';
+// noinspection ES6UnusedImports
+import { Command } from 'commander';
+// noinspection ES6UnusedImports
+import express from 'express';
+
+// Just some sample text 
+console.log('Sample program for the setup project');
