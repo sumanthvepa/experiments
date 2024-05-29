dc895b1eacb0dc367713b914645ac3b88cf2632e
Sumanth Vepa <svepa@milestone42.com>
2024-05-29 08:49:58 +0530

    [issue049] kotlin-basics: Explore comments, improve structure
    FEATURE(s)
    [issue049] kotlin-basics:
      Explore the basics of the kotlin programming language
    
    FILE(s)
    [new]kotlin/basics/.idea/codeStyles/codeStyleConfig.xml:
      Set indent to 2 spaces. Make that the project default
    [modified]kotlin/basics/.idea/misc.xml:
      Automated IDE changes.
    [modified]kotlin/basics/basics/build.gradle.kts:
      Upgrade dependencies for guava and junit
    [new]kotlin/basics/basics/src/main/kotlin/sv/basics/Comments.kt
      Explore comments in Kotlin
    [modified]kotlin/basics/basics/src/main/kotlin/sv/basics/Constants.kt
      Improve quality of comments and exploration
    [modified]kotlin/basics/basics/src/main/kotlin/sv/basics/Language.kt
      Improve comments, and call exploreComments
    [modified]kotlin/basics/basics/src/test/kotlin/sv/basics/LanguageTest.kt
      Replace test code with dummy test. This project does not
      need unit tests.
    
    DESCRIPTION
    Explore comments, improve structure

--- kotlin/basics/basics/src/main/kotlin/sv/basics/Constants.kt
+++ kotlin/basics/basics/src/main/kotlin/sv/basics/Constants.kt
@@ -22,8 +22,42 @@
  -------------------------------------------------------------------*/
 package sv.basics
 
+// Identifiers in Kotlin.
+// Kotlin does not support Unicode identifiers directly. (I think.)
+// This is the case for both constants and variables.
+
+// There are two types of constants in Kotlin  compile time constants
+// and runtime constants. A compile time constant is one whose value
+// is fully determined at compile time. This is indicated with
+// 'const val'.
+// For example reduced plank constant's value fully known at compile
+// time, so we can declare it as a const val.
 const val REDUCED_PLANCK_CONSTANT = 1.0545718e-34
 
+/**
+ * Explore constants in Kotlin
+ */
 fun exploreConstants() {
+    println("exploring constants...")
+
+    // Print a const val value.
     println("REDUCED_PLANK_CONSTANT = $REDUCED_PLANCK_CONSTANT")
+
+    // A dummy function that does not actually get user input. Used to
+    // demonstrate the concept of a runtime constant.
+    fun dummyReadLine(): String {
+        return "dummy input"
+    }
+
+    // A runtime constant is indicated with just val. It
+    // indicates a value that can be assigned to just once.
+    // In this sense it is very similar to Java's final
+    // keyword.
+    val input = dummyReadLine()
+    println("The value is $input")
+
+    // You cannot change the value of input once it is initialized.
+    // input = "new value" // Error: Val cannot be reassigned.
+
+    println("...finished exploring constants")
 }
