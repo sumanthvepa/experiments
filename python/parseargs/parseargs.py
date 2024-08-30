f89bd04765d1c0d2080230d689a1e03b0e3c2914
Sumanth Vepa <svepa@.milestone42.com>
2024-08-30 05:53:29 +0530

    [issue072] python-parseargs-update: Upgrade to python 3.12, latest packages, add copyleft
    FEATURE(s)
    [issue072] python-parseargs-update:
      Upgrade to python 3.12, the latest packages, add copyleft
    
    FILE(s)
    [modified]TODO.txt: Add issue072 as DONE
    [new]python/parseargs/COPYING:
      GNU GPL v3.0+ copyleft notice
    [new]python/parseargs/dependencies.sh:
      Script to install this projects dependencies
    [modified]python/parseargs/parseargs.py:
      Add copyleft notice to parsargs.
      Change shebang prompt to be more portable
    [new]python/parseargs/requirements.txt:
      Pip requirements file
    
    DESCRIPTION
    Upgrade to python 3.12, latest packages, add copyleft

--- python/parseargs/parseargs.py
+++ python/parseargs/parseargs.py
@@ -1,4 +1,25 @@
-#!/opt/local/bin/python3
+#!/usr/bin/env python3.12
+'''
+  parseargs.py: Explore the use of the argparse library
+'''
+# -------------------------------------------------------------------
+# parseargs.py: Explore the use of the argparse library
+#
+# Copyright (C) 2024 Sumanth Vepa.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# -------------------------------------------------------------------
 import argparse
 
 parser = argparse.ArgumentParser()
