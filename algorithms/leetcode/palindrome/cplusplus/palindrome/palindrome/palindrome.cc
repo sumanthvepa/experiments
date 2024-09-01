d08daf885edb7fabf8044ff35ac579bb7c1911e7
Sumanth Vepa <svepa@milestone42.com>
2024-09-02 01:08:32 +0530

    [issue076] palindrome-cpp: C++ solution to the palindrome leetcode problem
    FEATURE(s)
    [issue076] palindrome-cpp:
      C++ solution to the palindrome leetcode problem
    
    FILE(s)
    [modified]TODO.txt: Mark issue076 as DONE.
    [deleted]algorithms/leetcode/palindrome/cplusplus/palindrome/.idea/.gitignore:
      Deleted automatically by CLion
    [new]algorithms/leetcode/palindrome/cplusplus/palindrome/.idea/workspace.xml:
      CLion workspace. Automatically added by CLion
    [modified]algorithms/leetcode/palindrome/cplusplus/palindrome/Makefile:
      Ignore C++98 compatibility warnings
    [modified]algorithms/leetcode/palindrome/cplusplus/palindrome/palindrome.xcodeproj/project.pbxproj:
     Add Boost header and library search paths
     Ignore C++98 compatibility warnings
    [new]algorithms/leetcode/palindrome/cplusplus/palindrome/palindrome.xcodeproj/xcshareddata/xcschemes/palindrome.xcscheme:
      Add arguments to test the program from the command line
    [modified]algorithms/leetcode/palindrome/cplusplus/palindrome/palindrome/palindrome.cc:
      C++ solution to the palindrom leetcode problem
    
    DESCRIPTION
    C++ solution to the palindrome leetcode problem

--- algorithms/leetcode/palindrome/cplusplus/palindrome/palindrome/palindrome.cc
+++ algorithms/leetcode/palindrome/cplusplus/palindrome/palindrome/palindrome.cc
@@ -1,5 +1,36 @@
+// Ignore pedantic warnings from boost headers
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdouble-promotion"
+#pragma clang diagnostic ignored "-Wzero-as-null-pointer-constant"
+#pragma clang diagnostic ignored "-Wweak-vtables"
+#pragma clang diagnostic ignored "-Wpadded"
+#include <boost/lexical_cast.hpp>
+#pragma clang diagnostic pop
+
 #include <iostream>
 
-int main(int, const char * *) {
-  std::cout << "palindrome" << std::endl;
+static auto is_palindrome(int n) -> bool {
+  int reversed = 0;
+  int original = n;
+  while (n > 0) {
+    reversed = reversed * 10 + n % 10;
+    n /= 10;
+  }
+  return original == reversed;
 }
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
+auto main(int argc_, const char *argv_[]) -> int {
+  for (int n = 1; n < argc_; ++n) {
+    const char *arg = argv_[n];
+    try {
+      auto number = boost::lexical_cast<int>(arg);
+      std::cout << arg << (is_palindrome(number)? " is" : " is not") << " a palindrome\n";
+    } catch(const boost::bad_lexical_cast&) {
+      std::cerr << "'" << arg << "' is not an integer\n";
+    }
+  }
+  return 0;
+}
+#pragma clang diagnostic pop
