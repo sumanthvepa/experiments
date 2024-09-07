f875438eb6d9431248199bdc3daa406feabf0e67
Sumanth Vepa <svepa@milestone42.com>
2024-09-07 07:40:28 +0530

    [issue083] cplusplus-stdlibraries: Create the skeleton app
    FEATURE(s)
    [issue083] cplusplus-stdlibraries:
      Explore the C++ standard library
    
    FILE(s)
    [modified]TODO.txt: Add issue083
    [new]cplusplus/stdlibraries/.gitignore:
      Gitignore file for the directory
    [new]cplusplus/stdlibraries/.idea/.gitignore:
      Git ignore file for the directory. Generated by CLion
    [new]cplusplus/stdlibraries/.idea/misc.xml:
      Git ignore file for the directory. Generated by CLion
    [new]cplusplus/stdlibraries/.idea/vcs.xml:
      Git ignore file for the directory. Generated by CLion
    [new]cplusplus/stdlibraries/COPYING:
      GNU GPL v3.0+ copyleft notice
    [new]cplusplus/stdlibraries/Makefile:
      Standard makefile for the project
    [new]cplusplus/stdlibraries/stdlibraries.xcodeproj/project.pbxproj:
      Xcode project file. Generated by Xcode
    [new]cplusplus/stdlibraries/stdlibraries.xcodeproj/project.xcworkspace/contents.xcworkspacedata:
      Xcode project artifact. Generated by Xcode
    [new]cplusplus/stdlibraries/stdlibraries.xcodeproj/project.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist:
      Xcode project artifact. Generated by Xcode
    [new]cplusplus/stdlibraries/stdlibraries/iterators.cc:
      Explore <iterators> from the C++ standard library
    [new]cplusplus/stdlibraries/stdlibraries/iterators.hh:
      Header file to explore <iterators> from the C++ standard library
    [new]cplusplus/stdlibraries/stdlibraries/stdlibraries.cc:
      Explore the C++ standard library
    [new]cplusplus/stdlibraries/stdlibraries/streams.cc:
      Explore <iostreams> in the C++ standard library
    [new]cplusplus/stdlibraries/stdlibraries/streams.hh:
      Header file to explore <iostreams> in the C++ standard library
    [new]cplusplus/stdlibraries/stdlibraries/vectors.cc:
      Explore <vector> in the C++ standard library
    [new]cplusplus/stdlibraries/stdlibraries/vectors.hh:
      Header file to explore <vector> in the C++ standard library
    
    DESCRIPTION
    Create the skeleton app

---
+++ cplusplus/stdlibraries/stdlibraries/iterators.cc
@@ -0,0 +1,30 @@
+/**
+ * iterators.cc: Explore <iterators> in the C++ stdandard library
+ */
+/* -------------------------------------------------------------------
+ * iterators.cc: Explore <iterators> in the C++ stdandard library
+ *
+ * Copyright (C) 2024 Sumanth Vepa.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------*/
+#include <stdlibraries/iterators.hh>
+#include <iostream>
+
+auto sv::stdlibraries::explore_iterators() -> void {
+  std::cout << "explore_iterators()\n";
+  // TODO: Implement this.
+}
+
