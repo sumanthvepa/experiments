ed740272f4f858549656f0394617d601fd9de0de
Sumanth Vepa <svepa@.milestone42.com>
2024-09-08 09:17:49 +0530

    [issue085] selinux-basics: Setup the skeleton
    FEATURE(s)
    [issue085] selinux-basics: Explore SELinux
    
    FILE(s)
    [modified]TODO.txt
    [new]selinux/.gitignore
    [new]selinux/explore-selinux.sh
    
    DESCRIPTION
    Setup the skeleton

---
+++ selinux/explore-selinux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This shell script explore SELinux
+# The exploration is based on this blog post:
+# https://github.blog/developer-skills/programming-languages-and-frameworks/introduction-to-selinux/
