8634c0192fce8f7fa0d60a7b803f6a45653bb929
Sumanth Vepa <svepa@.milestone42.com>
2024-09-10 18:42:54 +0530

    [issue085] selinux-basics: Make not of things I want to explore
    FEATURE(s)
    [issue085] selinux-basics: Explore SELinux
    
    FILE(s)
    [modified]selinux/explore-selinux.sh:
      Add note to explore runuser and restoreconf
    
    DESCRIPTION
    Make not of things I want to explore

--- selinux/explore-selinux.sh
+++ selinux/explore-selinux.sh
@@ -3,3 +3,7 @@
 # This shell script explore SELinux
 # The exploration is based on this blog post:
 # https://github.blog/developer-skills/programming-languages-and-frameworks/introduction-to-selinux/
+
+# Explore runuser
+
+# Expore restorecon
