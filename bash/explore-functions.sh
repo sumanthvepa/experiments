5d2f475780fbea0745250f87dff8029996848593
Sumanth Vepa <svepa@.milestone42.com>
2024-09-12 18:21:02 +0530

    [issue086] bash-basics: Add placeholder to explore bash functions
    FEATURE(s)
    [issue086] bash-basics: Explore bash basics
    
    FILE(s)
    [new]bash/explore-functions.sh:
      Placeholder to explore bash functions
    
    DESCRIPTION
    Add placeholder to explore bash functions

---
+++ bash/explore-functions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# -------------------------------------------------------------------
+# explore-functions.sh: Explore functions in bash
+#
+# Copyright (C) 2024 Sumanth Vepa.
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License a
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see
+# <https://www.gnu.org/licenses/>.
+# -------------------------------------------------------------------
+
+echo "TODO: explore functions in bash"
+
+# in particular explore how to use functions from
+# within if statments. And how to use return etc.
+# See explore-funcname and ChatGPT logs for issues
+# that I should explore.
