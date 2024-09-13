a2c8e74400ffd96d7386ccab85b138c40d680bd7
Sumanth Vepa <svepa@.milestone42.com>
2024-09-13 18:17:54 +0530

    [issue086] bash-basics: Explore implicit 'in' clauses in for-loops and function declarations and parameter usage
    FEATURE(s)
    [issue086] bash-basics: Explore bash basics
    
    FILE(s)
    [modified]bash/explore-for-loops.sh:
      Explore implicit 'in' clauses in for-loops
    [modified]bash/explore-functions.sh:
      Explore function declarations and parameter usage
    
    DESCRIPTION
    Explore implicit 'in' clauses in for-loops and function declarations
    and parameter usage

--- bash/explore-functions.sh
+++ bash/explore-functions.sh
@@ -29,3 +29,62 @@ echo "TODO: explore functions in bash"
 # Explore variadic functions with $@, "$@" See
 # explore-command-line-arguments.sh for an example. Elaborate on that
 # example.
+
+# There are two ways to define functions in bash:
+# Standard function defintion
+# This uses the function keyword and has no parentheses
+# following it
+function foo {
+  echo "This is function foo"
+}
+
+# You can have a pair of parentheses following
+# it. This is the form I prefer. It makes it look
+# like a function.
+function foo1() {
+  echo "This is function foo1"
+}
+
+# Short form
+# I prefer the standard form with parenthesis.
+bar() {
+  echo "This is function bar"
+}
+
+# You can invoke a function in the same way as you would
+# a command:
+foo
+foo1
+bar
+
+# You can pass parameter to a function much the same
+# way as you would pass command line arguments to
+# a command
+
+# within a function you can access the individual
+# parameters as $1 $2 $3 upto $9.
+function bat() {
+  echo "$1 followed by $2, followed by $3"
+}
+
+bat first second third
+
+# $0 however always represents the name of the
+# script.
+function bat1() {
+  echo $0 # script name
+  echo $1 # first parameter
+}
+
+bat1 first
+
+# You can iterate over all the arguments using $@
+function bat2() {
+  local arg
+  for arg in "$@"; do
+    echo $arg
+  done
+}
+
+bat2 'first' 'the second one' 'third' 'fourth'
+
