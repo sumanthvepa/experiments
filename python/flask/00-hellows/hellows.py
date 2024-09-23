16cdfaa881ccb0f49e940e871a92f89a1bd75d48
Sumanth Vepa <svepa@milestone42.com>
2024-09-24 02:49:00 +0530

    [issue089] python-flask-00-hellows: A sweb service that returns 'hello'
    FEATURE(s)
    [issue089] python-flask-00-hellows:
      A simple hello web service that returns the string hello
    
    FILE(s)
    [modified]TODO.txt: Add issue089
    [new]python/flask/00-hellows/.gitignore:
      Ignore venv and __pycache__
    [new]python/flask/00-hellows/.idea/.gitignore:
      IntelliJ IDEA project artifact
    [new]python/flask/00-hellows/.idea/00-hellows.iml:
      IntelliJ IDEA project artifact
    [new]python/flask/00-hellows/.idea/misc.xml:
      IntelliJ IDEA project artifact
    [new]python/flask/00-hellows/.idea/modules.xml:
      IntelliJ IDEA project artifact
    [new]python/flask/00-hellows/.idea/vcs.xml:
      IntelliJ IDEA project artifact
    [new]python/flask/00-hellows/hellows.py:
      A simple hello web service that returns the string hello
    [new]python/flask/00-hellows/pylintrc:
      Pylintrc file for the project
    [new]python/flask/00-hellows/requirements.txt:
      Requirements file for the project
    [new]web/python/.gitignore:
      Git ignore file for the folder
    [new]web/python/flask:
      Symbolic link to the python/flask directory
    
    DESCRIPTION
    A sweb service that returns 'hello'

---
+++ python/flask/00-hellows/hellows.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3.12
+# -*- coding: utf-8 -*-
+"""
+  hellows.py: A simple flask application that sends the string 'hello'
+"""
+# -------------------------------------------------------------------
+# hellows.py: A simple flask application that sends the string 'hello'
+#
+# Copyright (C) 2024 Sumanth Vepa.
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License a
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see
+# <https://www.gnu.org/licenses/>.
+# -------------------------------------------------------------------
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+  return "hello"
