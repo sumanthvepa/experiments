731abeddcd6a82397605c6fd5f7eb6d2f0c44326
Sumanth Vepa <svepa@milestone42.com>
2025-04-07 19:53:16 +0530

    issue130-python-parse-dralithus-cmdline: Comment out all but the first no-args test
    FEATURE(s)
    issue130-python-parse-dralithus-cmdline: Experimental parser for the dralithus command line
    
    FILE(s)
    [modified]python/parse-dralithus-cmdline/test/test_parse_command_line.py:
      Comment out all but the first no-args test
    
    DESCRIPTION
    Comment out all but the first no-args test in TestParseCommandLine.
    This is to allow me to correct each test and the code being tested
    systematically.

--- python/parse-dralithus-cmdline/test/test_parse_command_line.py
+++ python/parse-dralithus-cmdline/test/test_parse_command_line.py
@@ -8,47 +8,47 @@ class TestParseCommandLine(unittest.TestCase):
         expected_positional: list[str] = []
         self.assertEqual(parse_command_line(args), (expected_options, expected_positional))
 
-    def test_single_short_option(self) -> None:
-        args: list[str] = ['-v']
-        expected_options: dict[str, int | bool] = {'v': 1, 'h': False}
-        expected_positional: list[str] = []
-        self.assertEqual(parse_command_line(args), (expected_options, expected_positional))
-
-    def test_single_long_option(self) -> None:
-        args: list[str] = ['--verbose']
-        expected_options: dict[str, int | bool] = {'v': 1, 'h': False}
-        expected_positional: list[str] = []
-        self.assertEqual(parse_command_line(args), (expected_options, expected_positional))
-
-    def test_combined_short_options(self) -> None:
-        args: list[str] = ['-vv']
-        expected_options: dict[str, int | bool] = {'v': 2, 'h': False}
-        expected_positional: list[str] = []
-        self.assertEqual(parse_command_line(args), (expected_options, expected_positional))
-
-    def test_option_with_value(self) -> None:
-        args: list[str] = ['--env', 'test']
-        expected_options: dict[str, int | bool | str] = {'v': 0, 'h': False, 'env': 'test'}
-        expected_positional: list[str] = []
-        self.assertEqual(parse_command_line(args), (expected_options, expected_positional))
-
-    def test_positional_arguments(self) -> None:
-        args: list[str] = ['application1', 'application2']
-        expected_options: dict[str, int | bool] = {'v': 0, 'h': False}
-        expected_positional: list[str] = ['application1', 'application2']
-        self.assertEqual(parse_command_line(args), (expected_options, expected_positional))
-
-    def test_mixed_arguments(self) -> None:
-        args: list[str] = ['-v', '--env', 'test', 'application1']
-        expected_options: dict[str, int | bool | str] = {'v': 1, 'h': False, 'env': 'test'}
-        expected_positional: list[str] = ['application1']
-        self.assertEqual(parse_command_line(args), (expected_options, expected_positional))
-
-    def test_help_option(self) -> None:
-        args: list[str] = ['-h']
-        expected_options: dict[str, int | bool] = {'v': 0, 'h': True}
-        expected_positional: list[str] = []
-        self.assertEqual(parse_command_line(args), (expected_options, expected_positional))
+    # def test_single_short_option(self) -> None:
+    #     args: list[str] = ['-v']
+    #     expected_options: dict[str, int | bool] = {'v': 1, 'h': False}
+    #     expected_positional: list[str] = []
+    #     self.assertEqual(parse_command_line(args), (expected_options, expected_positional))
+    #
+    # def test_single_long_option(self) -> None:
+    #     args: list[str] = ['--verbose']
+    #     expected_options: dict[str, int | bool] = {'v': 1, 'h': False}
+    #     expected_positional: list[str] = []
+    #     self.assertEqual(parse_command_line(args), (expected_options, expected_positional))
+    #
+    # def test_combined_short_options(self) -> None:
+    #     args: list[str] = ['-vv']
+    #     expected_options: dict[str, int | bool] = {'v': 2, 'h': False}
+    #     expected_positional: list[str] = []
+    #     self.assertEqual(parse_command_line(args), (expected_options, expected_positional))
+    #
+    # def test_option_with_value(self) -> None:
+    #     args: list[str] = ['--env', 'test']
+    #     expected_options: dict[str, int | bool | str] = {'v': 0, 'h': False, 'env': 'test'}
+    #     expected_positional: list[str] = []
+    #     self.assertEqual(parse_command_line(args), (expected_options, expected_positional))
+    #
+    # def test_positional_arguments(self) -> None:
+    #     args: list[str] = ['application1', 'application2']
+    #     expected_options: dict[str, int | bool] = {'v': 0, 'h': False}
+    #     expected_positional: list[str] = ['application1', 'application2']
+    #     self.assertEqual(parse_command_line(args), (expected_options, expected_positional))
+    #
+    # def test_mixed_arguments(self) -> None:
+    #     args: list[str] = ['-v', '--env', 'test', 'application1']
+    #     expected_options: dict[str, int | bool | str] = {'v': 1, 'h': False, 'env': 'test'}
+    #     expected_positional: list[str] = ['application1']
+    #     self.assertEqual(parse_command_line(args), (expected_options, expected_positional))
+    #
+    # def test_help_option(self) -> None:
+    #     args: list[str] = ['-h']
+    #     expected_options: dict[str, int | bool] = {'v': 0, 'h': True}
+    #     expected_positional: list[str] = []
+    #     self.assertEqual(parse_command_line(args), (expected_options, expected_positional))
 
 if __name__ == '__main__':
     unittest.main()
