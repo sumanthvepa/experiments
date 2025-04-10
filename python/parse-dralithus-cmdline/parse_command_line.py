666b9ad74482e8b698b847172693aaa88b07708b
Sumanth Vepa <svepa@milestone42.com>
2025-04-11 05:08:32 +0530

    issue130-python-parse-dralithus-cmdline: Remove helper functions to make commit sequence more intelligible
    FEATURE(s)
    issue130-python-parse-dralithus-cmdline: Experimental parser for the dralithus command line
    
    FILE(s)
    [modified]python/parse-dralithus-cmdline/parse_command_line.py:
      Remove helper functions to make commit sequence more intelligible
    
    DESCRIPTION
    Remove helper functions to make commit sequence more intelligible.

--- python/parse-dralithus-cmdline/parse_command_line.py
+++ python/parse-dralithus-cmdline/parse_command_line.py
@@ -1,53 +1,3 @@
-def is_option(arg: str) -> bool:
-  """
-    Check if the argument is an option.
-
-    :param arg: The argument to check.
-    :return True if the argument is an option, False otherwise.
-  """
-  return arg.startswith('-') and len(arg) > 1 and not arg[1].isdigit()
-
-
-def is_parameter(arg: str) -> bool:
-  """
-    Check if the argument is a parameter.
-    :param arg: The argument to check.
-    :return:  True if the argument is a parameter, False otherwise.
-  """
-  return len(arg) > 0 \
-    and not is_option(arg) \
-    and arg != '--' \
-    and not arg[0].isdigit() \
-    and arg[0] != '-'
-
-def option_name_and_value(arg: str, next_arg: str) -> tuple[str, bool | int | str | list[str], int]:
-  """
-    Get the option name and its value from the argument and the next argument
-
-    :param arg: The argument to check.
-    :param next_arg: The next argument, in case the current one requires a value.
-    :return: A tuple containing the option name and its value and how much to
-    increment the index by. The increment is 1 if the next argument is not the value
-    for the current argument, and 2 if it is.
-  """
-  # TODO: Implement this
-  return 'not-implemented', 'not-implemented', 1
-
-def add_to_options_dict(options: dict[str, str | int | bool ], option: str, value: str | int | bool) -> None:
-  """
-    Add an option and its value to the 'options' dictionary.
-
-    Do the right thing for the option being added:
-    - If the option is 'help', set it to True in the dictionary.
-    - If the option is 'verbosity', either increment the verbosity level or set it to the maximum of
-      the current verbosity level and the new value.
-    - If the option is 'env', set it to the value provided.
-    :param options: The options dictionary to update.
-    :param option:
-    :param value:
-    :return:
-  """
-
 def parse_command_line(args: list[str]) -> tuple[dict[str, str | int | bool], list[str]]:
   """
     Parses command-line arguments into options and positional parameters.
@@ -66,8 +16,8 @@ def parse_command_line(args: list[str]) -> tuple[dict[str, str | int | bool], li
     if is_option(arg):
       # increment is 1 if the next argument is not the value for the
       # current argument, and 2 if it is.
-      option, value, increment = option_name_and_value(arg, next_arg)
-      add_to_options_dict(options, option, value)
+      option_list, increment = get_options(arg, next_arg)
+      add_to_options_dict(options, option_list)
     elif is_parameter(arg):
       parameters.append(arg)
     else:
