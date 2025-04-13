0041cfe159f979f0cb5a28cd3d9de1231ab86630
Sumanth Vepa <svepa@milestone42.com>
2025-04-13 10:04:58 +0530

    issue130-python-parse-dralithus-cmdline: Define TestParseCommandLine.test_single_long_option_environment_with_value()
    FEATURE(s)
    issue130-python-parse-dralithus-cmdline: Experimental parser for the dralithus command line
    
    FILE(s)
    [modified]python/parse-dralithus-cmdline/test/test_parse_command_line.py:
      Define TestParseCommandLine.test_single_long_option_environment_with_value()
    
    DESCRIPTION
    Define TestParseCommandLine.test_single_long_option_environment_with_value().

--- python/parse-dralithus-cmdline/test/test_parse_command_line.py
+++ python/parse-dralithus-cmdline/test/test_parse_command_line.py
@@ -107,6 +107,15 @@ class TestParseCommandLine(unittest.TestCase):
     expected = (expected_options, expected_parameters)
     self.assertEqual(expected, actual)
 
+  def test_single_long_option_environment_with_value(self) -> None:
+    args: list[str] = ['--environment=local']
+    expected_options = {'verbosity': 0, 'help': True, 'environment': ['local']}
+    expected_parameters = []
+    actual = parse_command_line(args)
+    expected = (expected_options, expected_parameters)
+    self.assertEqual(expected, actual)
+
+
   #
   # def test_combined_short_options(self) -> None:
   #     args: list[str] = ['-vv']
