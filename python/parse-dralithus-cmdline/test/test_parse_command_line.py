1681754545e373a5d66e45f2543baa20a5936d11
Sumanth Vepa <svepa@milestone42.com>
2025-04-14 07:05:46 +0530

    issue130-python-parse-dralithus-cmdline: Define TestParseCommandLine.test_single_long_option_verbose_with_value()
    FEATURE(s)
    issue130-python-parse-dralithus-cmdline: Experimental parser for the dralithus command line
    
    FILE(s)
    [modified]python/parse-dralithus-cmdline/test/test_parse_command_line.py:
      Define TestParseCommandLine.test_single_long_option_verbose_with_value()
    
    DESCRIPTION
    Define TestParseCommandLine.test_single_long_option_verbose_with_value().

--- python/parse-dralithus-cmdline/test/test_parse_command_line.py
+++ python/parse-dralithus-cmdline/test/test_parse_command_line.py
@@ -91,6 +91,14 @@ class TestParseCommandLine(unittest.TestCase):
     expected = (expected_options, expected_parameters)
     self.assertEqual(expected, actual)
 
+  def test_single_long_option_verbose_with_value(self) -> None:
+    args: list[str] = ['--verbose=2']
+    expected_options = {'verbosity': 2, 'help': True, 'environment': []}
+    expected_parameters = []
+    actual = parse_command_line(args)
+    expected = (expected_options, expected_parameters)
+    self.assertEqual(expected, actual)
+
   def test_single_long_option_help(self) -> None:
     args: list[str] = ['--help']
     expected_options = {'verbosity': 0, 'help': True, 'environment': []}
