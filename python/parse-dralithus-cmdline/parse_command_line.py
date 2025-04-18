5710e526a156068fb994e16aae26ecd6f10ff85a
Sumanth Vepa <svepa@milestone42.com>
2025-04-18 08:19:27 +0530

    issue130-python-parse-dralithus-cmdline: Fix bug in get_short_option_name_and_value() where a short option for environment is provided with a value after a space
    FEATURE(s)
    issue130-python-parse-dralithus-cmdline: Experimental parser for the dralithus command line
    
    FILE(s)
    [modified]python/parse-dralithus-cmdline/parse_command_line.py:
      Fix bug in get_short_option_name_and_value() where a short option for
      environment is provided with a value after a space
    
    DESCRIPTION
    Fix bug in get_short_option_name_and_value() where a short option for
    environment is provided with a value after a space.

--- python/parse-dralithus-cmdline/parse_command_line.py
+++ python/parse-dralithus-cmdline/parse_command_line.py
@@ -218,7 +218,8 @@ def get_short_option_name_and_value(arg: str, next_arg: str) -> tuple[Option, in
       option = Option(option_name, option_value)
       increment = 1
     else:
-      if next_arg is not None and is_option_value(option_name, next_arg):
+      option_value = convert_to_type(option_value_type(option_name), next_arg)
+      if next_arg is not None and is_option_value(option_name, option_value):
         # Cast the value to the appropriate type
         option_value = convert_to_type(option_value_type(option_name), next_arg)
         option = Option(option_name, option_value)
