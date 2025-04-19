cda3e5b9b11b5e15155cca8d1e67cc9e6d6468fa
Sumanth Vepa <svepa@milestone42.com>
2025-04-19 09:13:41 +0530

    issue130-python-parse-dralithus-cmdline: Test multiple_options_verbosity_help_e_equal_environment_multi_space
    FEATURE(s)
    issue130-python-parse-dralithus-cmdline: Experimental parser for the dralithus command line
    
    FILE(s)
    [modified]python/parse-dralithus-cmdline/test/test_parse_command_line.py:
      Test multiple_options_verbosity_help_e_equal_environment_multi_space
    
    DESCRIPTION
    Test multiple_options_verbosity_help_e_equal_environment_multi_space.

--- python/parse-dralithus-cmdline/test/test_parse_command_line.py
+++ python/parse-dralithus-cmdline/test/test_parse_command_line.py
@@ -312,6 +312,11 @@ class TestParseCommandLine(unittest.TestCase):
         'args': ['-e', 'local', '-vvhv', '--environment=test,staging'],
         'expected_options': {'verbosity': 3, 'help': True, 'environment': ['local', 'test', 'staging']},
         'expected_parameters': []
+      }),
+      ('multiple_options_verbosity_help_e_equal_environment_multi_space', {
+        'args': ['-e=local', '-vvvh', '--environment', 'test,staging'],
+        'expected_options': {'verbosity': 3, 'help': True, 'environment': ['local', 'test', 'staging']},
+        'expected_parameters': []
       })
     ]
 
