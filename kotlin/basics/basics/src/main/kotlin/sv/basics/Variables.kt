d23720f5378d38dfdebb644b0bcf732997890274
Sumanth Vepa <svepa@milestone42.com>
2025-06-12 14:51:50 +0530

    [issue049] kotlin-basics: Skeleton file to explore variables
    FEATURE(s)
    [issue049] kotlin-basics:
      Explore the basics of the kotlin programming language
    
    FILE(s)
    [new]kotlin/basics/basics/src/main/kotlin/sv/basics/Variables.kt:
      Skeleton file to explore variables
    
    DESCRIPTION
    Skeleton file to explore variables

---
+++ kotlin/basics/basics/src/main/kotlin/sv/basics/Variables.kt
@@ -0,0 +1,24 @@
+/**
+ * @file Variables.kt: Explore variables in Kotlin.
+ */
+/* -------------------------------------------------------------------
+ * Variables.kt: Explore variables in Kotlin.
+ *
+ * Copyright (C) 2024-25 Sumanth Vepa.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------*/
+package sv.basics
+
