ec29356f66132bb1495e1327a17017b6e939c617
Sumanth Vepa <svepa@.milestone42.com>
2025-07-06 16:04:01 +0530

    issue144-python-testing: Add notes on how to create stub files
    FEATURE(s)
    issue144-python-testing: Add notes on how to create stub files
    
    FILE(s)
    [modified]TODO.txt:
      Add issue 144 as DONE
    [new]python/testing/README.md:
      Add notes on how to create stub files
    [modified]python/testing/stubs/parameterized/__init__.pyi:
      Type annotations for parameterized.expand
    
    DESCRIPTION
    Add notes on how to create stub files.
    Also update type annotations for parameterized.expand

--- python/testing/stubs/parameterized/__init__.pyi
+++ python/testing/stubs/parameterized/__init__.pyi
@@ -1,3 +1,16 @@
-from .parameterized import param as param, parameterized as parameterized, parameterized_class as parameterized_class
+# from .parameterized import param as param, parameterized as parameterized, parameterized_class as parameterized_class
 
-__version__: str
+from typing import Any, Callable, Iterable
+
+# name_func: Callable[[Any], str] | None = None,
+# doc_func: Callable[[Any], str] | None = None,
+# skip_on_empty: bool = False,
+# namespace: dict[str, Any] | None = None) \
+
+
+# noinspection PyPep8Naming
+class parameterized:
+  @classmethod
+  def expand(cls, input: Iterable[Any]) \
+      -> Callable[..., Any]:
+    pass  # Implementation would go here
