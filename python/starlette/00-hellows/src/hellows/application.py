5560398914765785115a43719e9946485b674ca7
Sumanth Vepa <svepa@milestone42.com>
2025-08-14 09:13:14 +0530

    issue150-python-starlette-00-hellows: Update packages and IntelliJ IDEA project
    FEATURE(s)
    issue150-python-starlette-00-hellows:
      A hello world web service using starlette
    
    FILE(s)
    [modified]python/starlette/00-hellows/.idea/00-hellows.iml:
      Exclude venv directory. Automatically generated by IntelliJ IDEA
    [modified]python/starlette/00-hellows/.idea/misc.xml:
      Set the Python SDK to the one in venv. Automatically generated by IntelliJ IDEA
    [new]python/starlette/00-hellows/.idea/pylint.xml:
      Create pylint plugin configuration. Automatically generated by IntelliJ IDEA
    [modified]python/starlette/00-hellows/requirements.txt:
      Upgrade pylint dependency
    [modified]python/starlette/00-hellows/src/hellows/application.py
      Add a note on problems one might encounter on Chrome
    
    DESCRIPTION
    Update packages and IntelliJ IDEA project.

--- python/starlette/00-hellows/src/hellows/application.py
+++ python/starlette/00-hellows/src/hellows/application.py
@@ -26,4 +26,13 @@ app = Starlette(debug=True, routes=[
 
 if __name__ == '__main__':
   import uvicorn
+  # Note that Chrome will not allow you to connect to a web service
+  # running on certain ports. 5100 is not one of those ports, so you
+  # okay here. But ports like 6000 will be blocked with an
+  # ERR_UNSAFE_PORT.
+  # To overcome this you need to either use a different browser or
+  # launch Chrome from the command line as follows (on the Mac):
+  # open -na "Google Chrome" --args --explicitly-allowed-ports=6000 --profile-directory="<Profile Directory Name>"
+  # The profile directory name can be "Default" or One of the profile
+  # directories listed at ~/Library/Application Support/Google/Chrome/
   uvicorn.run(app, host='0.0.0.0', port=5100)
