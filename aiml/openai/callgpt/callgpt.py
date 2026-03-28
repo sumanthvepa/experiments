9798596b0f173c30f15f475ef7c5ffb65ca3cdde
Sumanth Vepa <svepa@milestone42.com>
2026-03-28 10:39:45 +0530

    issue162-move-aiml-into-math-cs: Move aiml into the mathematics/computer-science/folder
    FEATURE(s)
    issue162-move-aiml-into-math-cs: Move aiml into the mathematics/computer-science/folder
    issue161-cleanup-math-folder: Clean up the math folder
    
    FILE(s)
    [modified]TODO.txt
    [modified]aiml/openai/callgpt/callgpt.py
    [new]aiml/openai/callgpt/local-packages.txt
    [new]aiml/openai/callgpt/packages.txt
    [modified]aiml/openai/callgpt/requirements.txt
    
    DESCRIPTION
    Commit changes to the aiml folder

--- aiml/openai/callgpt/callgpt.py
+++ aiml/openai/callgpt/callgpt.py
@@ -24,17 +24,11 @@
 import os
 from openai import OpenAI
 
-client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
 
-COMPLETION = client.chat.completions.create(
-  model='gpt-3.5-turbo',
-  messages=[
-    {'role': 'system', 'content': "You are a large language model."},
-    {'role': 'user', 'content': 'Write a Python function is_prime ' +
-                                'that returns true if its input is ' +
-                                'a prime number'}
-  ]
-)
-
-response = COMPLETION.choices[0].message.content
-print(response)
+if __name__ == '__main__':
+  client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
+  response = client.responses.create(
+    model='gpt-5-nano',
+    input='Write a Python function is prime ' +
+          'that returns true if its input is a prime number')
+  print(response.output_text)
