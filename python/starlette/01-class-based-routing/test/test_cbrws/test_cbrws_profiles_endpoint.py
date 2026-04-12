c256e60e8dbd067a1248ce6d63f7a4aa06d69251
Sumanth Vepa <svepa@milestone42.com>
2026-04-12 08:55:38 +0530

    issue187: Add tests for the CBRWS profile directory endpoint
    FEATURE(s)
    issue187-python-starlette-01-cbrws-cbrws-profile-endpoint-tests:
      Add tests for the /profiles/cbrws endpoint
    
    FILE(s)
    [modified]TODO.txt:
      Mark issue187 as DONE
    [new]python/starlette/01-class-based-routing/test/test_cbrws/test_cbrws_profiles_endpoint.py:
      Add unit tests for the /profiles/cbrws endpoint.
      Check content negotiation for HAL JSON and HTML responses.
      Compare returned content against the rendered templates.
      Check HEAD, OPTIONS, Link, Allow, and 406 responses.
    
    DESCRIPTION
    This commit adds focused tests for the /profiles/cbrws directory URL.
    The tests verify response metadata and rendered content so the
    profile version directory contract is covered beyond status code
    checks.

---
+++ python/starlette/01-class-based-routing/test/test_cbrws/test_cbrws_profiles_endpoint.py
@@ -0,0 +1,184 @@
+"""
+  test_cbrws_profiles_endpoint.py: Unit tests for the /profiles/cbrws
+  endpoint of the cbrws webservice.
+"""
+import asyncio
+import unittest
+
+from httpx import Response
+from starlette import status
+
+from cbrws.cbrws_profiles_endpoint import CBRWSProfilesEndpoint
+from test_cbrws.link_header import Link, parse
+from test_cbrws.test_helper import TestHelper
+
+
+class TestCBRWSProfilesEndpoint(unittest.TestCase, TestHelper):
+  """
+    Unit tests for the CBRWS profile versions directory route.
+  """
+  @property
+  def cbrws_profile_url(self) -> str:
+    """
+      Expected URL for the CBRWS profile family.
+      :return: The CBRWS profile family URL
+    """
+    return f'{self.base_url}/profiles/cbrws'
+
+  @property
+  def cbrws_v1_profile_url(self) -> str:
+    """
+      Expected URL for the CBRWS v1 profile.
+      :return: The CBRWS v1 profile URL
+    """
+    return f'{self.base_url}/profiles/cbrws/v1'
+
+  @property
+  def hal_media_type(self) -> str:
+    """
+      Expected media type for HAL responses.
+      :return: The HAL media type
+    """
+    return 'application/hal+json'
+
+  @property
+  def template_context(self) -> dict[str, str]:
+    """
+      Expected template context for the CBRWS profile versions directory.
+      :return: The template context
+    """
+    return {
+      'cbrws_profile_url': self.cbrws_profile_url,
+      'cbrws_v1_profile_url': self.cbrws_v1_profile_url
+    }
+
+  def check_cbrws_profiles_link(self, response: Response) -> None:
+    """
+      Check that the response has the CBRWS profile directory Link header.
+      :param response: The response object
+      :return: None
+    """
+    self.assertIn('Link', response.headers)
+    actual_links: dict[str, Link] = parse(response.headers['Link'])
+    expected_links: dict[str, Link] = {
+      'self': Link(
+        url=self.cbrws_profile_url,
+        rel='self',
+        media_type=self.hal_media_type),
+      'item': Link(
+        url=self.cbrws_v1_profile_url,
+        rel='item',
+        media_type=self.schema_media_type,
+        title='CBRWS v1 API profile')
+    }
+    for rel, link in expected_links.items():
+      self.assertIn(rel, actual_links)
+      self.assertEqual(link, actual_links[rel])
+
+  def test_get_hal_json_by_default(self) -> None:
+    """
+      Test that GET /profiles/cbrws returns HAL JSON by default.
+      :return: None
+    """
+    response = self.make_request('GET', '/profiles/cbrws')
+    self.assertEqual(status.HTTP_200_OK, response.status_code)
+    self.check_content_type(response, self.hal_media_type)
+    self.check_allow(response)
+    self.check_cbrws_profiles_link(response)
+    expected_data = asyncio.run(CBRWSProfilesEndpoint.load_json(
+      str(CBRWSProfilesEndpoint.SCHEMA_DIR / 'cbrws-profiles.json'),
+      self.template_context))
+    self.assertDictEqual(response.json(), expected_data)
+
+  def test_get_hal_json_with_accept_header(self) -> None:
+    """
+      Test that GET /profiles/cbrws returns HAL JSON when requested.
+      :return: None
+    """
+    response = self.make_request(
+      'GET',
+      '/profiles/cbrws',
+      headers={'Accept': self.hal_media_type})
+    self.assertEqual(status.HTTP_200_OK, response.status_code)
+    self.check_content_type(response, self.hal_media_type)
+    self.check_allow(response)
+    self.check_cbrws_profiles_link(response)
+    expected_data = asyncio.run(CBRWSProfilesEndpoint.load_json(
+      str(CBRWSProfilesEndpoint.SCHEMA_DIR / 'cbrws-profiles.json'),
+      self.template_context))
+    self.assertDictEqual(response.json(), expected_data)
+
+  def test_get_html(self) -> None:
+    """
+      Test that GET /profiles/cbrws returns HTML when requested.
+      :return: None
+    """
+    response = self.make_request(
+      'GET',
+      '/profiles/cbrws',
+      headers={'Accept': 'text/html'})
+    self.assertEqual(status.HTTP_200_OK, response.status_code)
+    self.check_content_type(response, 'text/html; charset=utf-8')
+    self.check_allow(response)
+    self.check_cbrws_profiles_link(response)
+    expected_html = asyncio.run(CBRWSProfilesEndpoint.load_file(
+      str(CBRWSProfilesEndpoint.SCHEMA_DIR / 'cbrws-profiles.jinja2'),
+      self.template_context))
+    self.assertEqual(expected_html, response.text)
+
+  def test_get_unsupported_media_type(self) -> None:
+    """
+      Test that unsupported Accept values return 406.
+      :return: None
+    """
+    response = self.make_request(
+      'GET',
+      '/profiles/cbrws',
+      headers={'Accept': 'application/xml'})
+    self.assertEqual(status.HTTP_406_NOT_ACCEPTABLE, response.status_code)
+    self.check_content_type(response, self.problem_media_type)
+    self.check_allow(response)
+    self.check_cbrws_profiles_link(response)
+    data = response.json()
+    self.assertEqual('Not Acceptable', data['title'])
+    self.assertEqual(status.HTTP_406_NOT_ACCEPTABLE, data['status'])
+
+  def test_head_hal_json(self) -> None:
+    """
+      Test that HEAD /profiles/cbrws returns HAL JSON headers.
+      :return: None
+    """
+    response = self.make_request(
+      'HEAD',
+      '/profiles/cbrws',
+      headers={'Accept': self.hal_media_type})
+    self.assertEqual(status.HTTP_200_OK, response.status_code)
+    self.check_content_type(response, self.hal_media_type)
+    self.check_allow(response)
+    self.check_cbrws_profiles_link(response)
+    self.assertEqual('', response.text)
+
+  def test_head_html(self) -> None:
+    """
+      Test that HEAD /profiles/cbrws returns HTML headers when requested.
+      :return: None
+    """
+    response = self.make_request(
+      'HEAD',
+      '/profiles/cbrws',
+      headers={'Accept': 'text/html'})
+    self.assertEqual(status.HTTP_200_OK, response.status_code)
+    self.check_content_type(response, 'text/html; charset=utf-8')
+    self.check_allow(response)
+    self.check_cbrws_profiles_link(response)
+    self.assertEqual('', response.text)
+
+  def test_options_cbrws_profiles(self) -> None:
+    """
+      Test that OPTIONS /profiles/cbrws returns allowed methods and links.
+      :return: None
+    """
+    response = self.make_request('OPTIONS', '/profiles/cbrws')
+    self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+    self.check_allow(response)
+    self.check_cbrws_profiles_link(response)
